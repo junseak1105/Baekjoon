15d03c0f5f425b2c8ded0b781f05a1467565bb00
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-07-27 17:17:18 +0900

    Q1259 진행중

---
+++ src/Bronze_I/Q1259.java
@@ -0,0 +1,20 @@
+package Bronze_I;
+import java.util.*;
+
+public class Q1259 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        int input = 0;
+        do{
+            input = in.nextInt();
+            arrayList.add(input);
+        }while(input!=0);    
+
+        for (int i = 0; i < arrayList.size()-1; i++) {
+            String str = Integer.toString(arrayList.get(i));
+            
+        }
+    }
+    
+}
